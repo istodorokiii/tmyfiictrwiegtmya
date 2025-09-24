8e342bf9248762b6cdecd1a1fd9adf9657523f26
istodorokiii <kiritoxkazuto0710@gmail.com>
2025-09-24 21:25:43 +0800

    press between play and pause button

--- interactive/KimDokja.js
+++ interactive/KimDokja.js
@@ -1,15 +1,17 @@
 const music = document.getElementById("eternity");
 const playButton = document.querySelector(".play");
+const pauseButton = document.querySelector(".pause"); // make sure this exists in HTML!
 
-playButton.addEventListener("click"),
-  () => {
-    if (music.paused) {
-      music.play();
-      playButton.classList.remove("fa-solid fa-play");
-      playButton.classList.add("fa-solid fa-pause");
-    } else {
-      music.pause();
-      playButton.classList.remove("fa-solid fa-pause");
-      playButton.classList.add("fa-solid fa-play");
-    }
-  };
+playButton.addEventListener("click", () => {
+  music.paused;
+  music.play();
+  playButton.style.display = "none";
+  pauseButton.style.display = "block";
+});
+
+pauseButton.addEventListener("click", () => {
+  !music.paused;
+  music.pause();
+  pauseButton.style.display = "none";
+  playButton.style.display = "block";
+});
