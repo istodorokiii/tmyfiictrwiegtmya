c095896a199355c28bb76a48b6f3cbeacdc40dcd
istodorokiii <kiritoxkazuto0710@gmail.com>
2025-09-28 21:45:33 +0800

    made the progress bar fill while song is playing

--- interactive/KimDokja.js
+++ interactive/KimDokja.js
@@ -17,3 +17,27 @@ pauseButton.addEventListener("click", () => {
     playButton.style.display = "inline-block";
   }
 });
+
+const progressFill = document.getElementById("progress-fill");
+const progressDot = document.getElementById("progress-dot");
+const currentTime = document.querySelector("#time-info span:first-child");
+const totalDuration = document.querySelector("#time-info span:last-child");
+
+function formatTime(sec) {
+  if (isNaN(sec)) return "0:00";
+  const minutes = Math.floor(sec / 60);
+  const seconds = Math.floor(sec % 60)
+    .toString()
+    .padStart(2, "0");
+  return `${minutes}:${seconds}`;
+}
+
+music.addEventListener("loadedmetadata", () => {
+  totalDuration.textContent = formatTime(music.duration);
+});
+
+music.addEventListener("timeupdate", () => {
+  const progressPercent = (music.currentTime / music.duration) * 100;
+  progressFill.style.width = `${progressPercent}%`;
+  currentTime.textContent = formatTime(music.currentTime);
+});
