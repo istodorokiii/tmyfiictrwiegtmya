73312914fa37be537b99176776e6cf620395fab7
istodorokiii <kiritoxkazuto0710@gmail.com>
2025-10-03 21:53:41 +0800

    music player is able to skip to desired part of song

--- interactive/KimDokja.js
+++ interactive/KimDokja.js
@@ -2,6 +2,13 @@ const music = document.getElementById("eternity");
 const playButton = document.querySelector(".play");
 const pauseButton = document.querySelector(".pause");
 
+const progressFill = document.getElementById("progress-fill");
+const progressDot = document.getElementById("progress-dot");
+const currentTime = document.querySelector("#time-info span:first-child");
+const totalDuration = document.querySelector("#time-info span:last-child");
+
+const progressBar = document.getElementById("progress-bar");
+
 playButton.addEventListener("click", () => {
   if (music.paused) {
     music.play();
@@ -17,11 +24,6 @@ pauseButton.addEventListener("click", () => {
     playButton.style.display = "inline-block";
   }
 });
-
-const progressFill = document.getElementById("progress-fill");
-const progressDot = document.getElementById("progress-dot");
-const currentTime = document.querySelector("#time-info span:first-child");
-const totalDuration = document.querySelector("#time-info span:last-child");
 
 function formatTime(sec) {
   if (isNaN(sec)) return "0:00";
@@ -40,4 +42,15 @@ music.addEventListener("timeupdate", () => {
   const progressPercent = (music.currentTime / music.duration) * 100;
   progressFill.style.width = `${progressPercent}%`;
   currentTime.textContent = formatTime(music.currentTime);
+
+  progressDot.style.left = `calc(${progressPercent}% - 7)`;
+
+  currentTime.textContent = formatTime(music.currentTime);
+});
+
+progressBar.addEventListener("click", (e) => {
+  const barWidth = progressBar.clientWidth;
+  const clickX = e.offsetX;
+  const seekTime = (clickX / barWidth) * music.duration;
+  music.currentTime = seekTime;
 });
